2c418d90f04d8855c14356fb05db15a0f37bb632
viowl <sovaworkbox@gmail.com>
2024-02-28 21:55:16 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
+
 const App = () => {
+  const [selectedIdx, setSelectedIdx] = useState(0);
+
+const = handlePrev = () => {
+  
+}
+
   return (
-    <>
-      <div>Hello</div>
+    <div>
+      <div>
+        <button>Prev</button>
+        <button>Next</button>
+      </div>
+      <p>0/0</p>
+      <article>
+        <h1>Topic</h1>
+        <p>Text</p>
+      </article>
       {console.log(12)}
-    </>
+    </div>
   );
 };
 
